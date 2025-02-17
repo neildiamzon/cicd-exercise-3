fef60bfd3d1f7881bffba805a6e8400f41594e2b
neil <neilaronndiamzon@gmail.com>
2025-02-17 16:27:47 +1300

    initial commit

---
+++ notes/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+
+from notes.models import Note
+
+# Register your models here.
+
+
+admin.site.register(Note)
---
+++ notes/models.py
@@ -0,0 +1,15 @@
+# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+class Note(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+
